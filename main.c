c28f2c89b0ce66653c0e6d794e04d717356f422a
Mona <munaaljaali9375@gmail.com>
2023-12-14 18:58:29 -0800

    fix

--- main.c
+++ main.c
@@ -31,6 +31,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	helper(file, choice, stack);
+	helper(file, choice, &stack);
+	fclose(file);
 	return (0);
 }
